dc7f750f092804c0ae710768bd71e615f56bf0e6
wangmingshun <461514274@qq.com>
2016-07-24 22:56:32 +0800

    aa

---
+++ POS/src/main/test/TestSuite.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * 测试序列 test
+ * 
+ * @author xiongyuehua
+ * @since 2010-11-5
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class TestSuite {
+
+}
